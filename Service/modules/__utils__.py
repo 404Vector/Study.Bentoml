5ee5ec15e885b8d29729b527a5a5d215864e7f64
HSKim <tiryul@gmail.com>
2022-11-12 12:53:55 +0000

    update utils: transform_image

--- Service/modules/__utils__.py
+++ Service/modules/__utils__.py
@@ -1,81 +1,23 @@
 import io
-
 import albumentations
 import albumentations.pytorch
 import numpy as np
 import torch
 from PIL import Image
-import streamlit as st
-import collections
-import functools
-import inspect
-import textwrap
-
-
-def cache_on_button_press(label, **cache_kwargs):
-    """Function decorator to memoize function executions.
-    Parameters
-    ----------
-    label : str
-        The label for the button to display prior to running the cached funnction.
-    cache_kwargs : Dict[Any, Any]
-        Additional parameters (such as show_spinner) to pass into the underlying @st.cache decorator.
-    Example
-    -------
-    This show how you could write a username/password tester:
-    >>> @cache_on_button_press('Authenticate')
-    ... def authenticate(username, password):
-    ...     return username == "buddha" and password == "s4msara"
-    ...
-    ... username = st.text_input('username')
-    ... password = st.text_input('password')
-    ...
-    ... if authenticate(username, password):
-    ...     st.success('Logged in.')
-    ... else:
-    ...     st.error('Incorrect username or password')
-    """
-    internal_cache_kwargs = dict(cache_kwargs)
-    internal_cache_kwargs['allow_output_mutation'] = True
-    internal_cache_kwargs['show_spinner'] = False
-
-    def function_decorator(func):
-        @functools.wraps(func)
-        def wrapped_func(*args, **kwargs):
-            @st.cache(**internal_cache_kwargs)
-            def get_cache_entry(func, args, kwargs):
-                class ButtonCacheEntry:
-                    def __init__(self):
-                        self.evaluated = False
-                        self.return_value = None
-
-                    def evaluate(self):
-                        self.evaluated = True
-                        self.return_value = func(*args, **kwargs)
-
-                return ButtonCacheEntry()
-
-            cache_entry = get_cache_entry(func, args, kwargs)
-            if not cache_entry.evaluated:
-                if st.button(label):
-                    cache_entry.evaluate()
-                else:
-                    st.stop()
-            return cache_entry.return_value
-
-        return wrapped_func
-
-    return function_decorator
     
-def transform_image(image_bytes: bytes) -> torch.Tensor:
+def transform_image(data) :
     transform = albumentations.Compose([
             albumentations.Resize(height=384, width=384),
             albumentations.Normalize(mean=(0.5, 0.5, 0.5),
                                      std=(0.2, 0.2, 0.2)),
             albumentations.pytorch.transforms.ToTensorV2()
         ])
-    image = Image.open(io.BytesIO(image_bytes))
-    image = image.convert('RGB')
-    image_array = np.array(image)
-    return transform(image=image_array)['image'].unsqueeze(0)
-
+    image_array = np.array(data, np.float32)
+    images = []
+    for img in image_array:
+        result = transform(image=img)['image']
+        images.append(result)
+    images = torch.cat(images)
+    if images.dim() == 3:
+        images = images.unsqueeze(0)
+    return images
